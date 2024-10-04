a94b78cc5ceda1d45c7bb0c39b21cec42e095fa9
rajeshbolisetty <rajesh.bnv@gmail.com>
2024-10-04 14:42:18 +1300

    fix some code

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 export function info(message, messageContext = "", messageType = "") {
-  let messageHeaders = getMessageContext(messageContext, messageType);
-  console.log("INFO: " + messageHeaders + message);
+  const messageHeaders = getMessageContext(messageContext, messageType);
+  console.info(`INFO: ${messageHeaders}${message}`);
 }
 
 export function warn(message, messageContext = "", messageType = "") {
-  let messageHeaders = getMessageContext(messageContext, messageType);
-  console.log("WARN: " + messageHeaders + message);
+  const messageHeaders = getMessageContext(messageContext, messageType);
+  console.warn(`WARN: ${messageHeaders}${message}`);
 }
 
 export function debug(message, messageContext = "", messageType = "") {
-  let messageHeaders = getMessageContext(messageContext, messageType);
-  console.log("DEBUG: " + messageHeaders + message);
+  const messageHeaders = getMessageContext(messageContext, messageType);
+  console.debug(`DEBUG: ${messageHeaders}${message}`);
 }
 
 export function error(message, messageContext = "", messageType = "") {
-  let messageHeaders = getMessageContext(messageContext, messageType);
+  const messageHeaders = getMessageContext(messageContext, messageType);
   if (typeof message === "object" && message !== null) {
-    console.error(
-      "ERROR: " + messageHeaders + (message.stack || JSON.stringify(message))
-    );
+    console.error(`ERROR: ${messageHeaders}${message.stack || JSON.stringify(message)}`);
   } else {
-    console.error("ERROR: " + messageHeaders + message);
+    console.error(`ERROR: ${messageHeaders}${message}`);
   }
 }
 
 function getMessageContext(messageContext, messageType = "") {
   if (messageContext || messageType) {
-    return messageContext + " " + messageTypes[messageType];
+    return `${messageContext} ${messageTypes[messageType]}`;
   }
-
   return "";
 }
+
+const messageTypes = {
+  REQUEST: "Request",
+  RESPONSE: "Response",
+};
